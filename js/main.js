26c98777c437fa94fe444951707bf6293f9c6f6b
David Gomez <dagove47@gmail.com>
2022-06-05 03:29:30 -0600

    Add card functionality
    Add card functionality is working

--- js/main.js
+++ js/main.js
@@ -12,6 +12,12 @@
     const newEmail = document.querySelector("#newEmail");
     const newAddress = document.querySelector("#newAddress");
 
+    const contactsContainer = document.querySelector("[data-contacts-container]");
+    const contactCardTemplate = document.querySelector("[data-contact-template]");
+    
+    const deleteContactBtn = document.getElementById('deleteContactBtn');
+    const editContactBtn = document.getElementById('editContactBtn');
+
     
     function openCloseModal() {
         addContactModal.classList.toggle("OpenModal");
@@ -33,6 +39,39 @@
         openCloseModal();
     })
 
+    let allCards = [];
+
+    class Card {
+        constructor(name, phone, email, address) {
+            this.name = name;
+            this.phone = phone;
+            this.email = email;
+            this.address = address;
+        }
+    }
+
+    function addContactInfo (name, phone, email, address) {
+        let newCard = new Card(name, phone, email, address);
+        allCards.push(newCard);
+    }
+
+    function createCards(data) {
+
+        const card = contactCardTemplate.content.cloneNode(true).children[0];
+
+        const nameContactCard = card.querySelector("[data-name]");
+        const phoneContactCard = card.querySelector("[data-phone]");
+        const emailContactCard = card.querySelector("[data-email]");
+        const addressContactCard = card.querySelector("[data-address]");
+
+        nameContactCard.textContent = data[data.length -1].name;
+        phoneContactCard.textContent = data[data.length -1].phone;
+        emailContactCard.textContent = data[data.length -1].email;
+        addressContactCard.textContent = data[data.length -1].address;
+
+        contactsContainer.appendChild(card);
+
+    }
 
     formNewContact.addEventListener("submit", (e) => {
         e.stopPropagation();
@@ -45,14 +84,29 @@
             formNewContact.classList.add('was-validated');
 
         } else {
-
+            let nameValue = newName.value;
+            let phoneValue = newPhone.value;
+            let emailValue = newEmail.value;
+            let addressValue = newAddress.value;
+            addContactInfo(nameValue, phoneValue, emailValue, addressValue);
             formInputs.forEach(input => input.value = '');
             formNewContact.classList.remove('was-validated');
             openCloseModal();
-
+            createCards(allCards);
+            console.log(allCards);
+            console.log(allCards[0]);
+            console.log(allCards[0].name);
         }
         
     })
+
+    // deleteContactBtn.addEventListener("click", (e) => {
+    //     e.stopPropagation();
+    //     console.log("");
+    // })
+
+
+    
 
 })()
 
