411a08638079651d16d857f426323a9da6fdddd7
David Gomez <dagove47@gmail.com>
2022-06-15 00:59:16 -0600

    Card creation updated and Token
    Card creation updated, basic token id added and html structure refined

--- js/main.js
+++ js/main.js
@@ -1,62 +1,68 @@
-(() => {
-    'use strict'
+const addContactModal = document.getElementById('addContactModal');
 
-    const addContactModal = document.getElementById('addContactModal');
-    const modalBox = document.getElementById('modalBox');
-    const openModalButton = document.querySelector("#openModalButton");
-    const closeModalButton = document.querySelector("#closeModalButton");
+const modalBox = document.getElementById('modalBox');
+const openModalButton = document.querySelector("#openModalButton");
+const closeModalButton = document.querySelector("#closeModalButton");
 
-    const formNewContact = document.querySelector("#formNewContact");
-    const newName = document.querySelector("#newName");
-    const newPhone = document.querySelector("#newPhone");
-    const newEmail = document.querySelector("#newEmail");
-    const newAddress = document.querySelector("#newAddress");
+const formNewContact = document.querySelector("#formNewContact");
+const newName = document.querySelector("#newName");
+const newPhone = document.querySelector("#newPhone");
+const newEmail = document.querySelector("#newEmail");
+const newAddress = document.querySelector("#newAddress");
 
-    const contactsContainer = document.querySelector("[data-contacts-container]");
-    const contactCardTemplate = document.querySelector("[data-contact-template]");
-    
-    const deleteContactBtn = document.getElementById('deleteContactBtn');
-    const editContactBtn = document.getElementById('editContactBtn');
+const contactsContainer = document.querySelector("[data-contacts-container]");
+const contactCardTemplate = document.querySelector("[data-contact-template]");
 
-    
-    function openCloseModal() {
-        addContactModal.classList.toggle("OpenModal");
+const deleteContactBtn = document.getElementById('deleteContactBtn');
+const editContactBtn = document.getElementById('editContactBtn');
+
+
+const allCards = [];
+
+
+function openCloseModal() {
+    addContactModal.classList.toggle("OpenModal");
+}
+
+openModalButton.addEventListener("click", (e) => {
+    e.stopPropagation();
+    openCloseModal();
+})
+modalBox.addEventListener("click", (e) => {
+    e.stopPropagation();
+})
+closeModalButton.addEventListener("click", (e) => {
+    e.stopPropagation();
+    openCloseModal();
+})
+addContactModal.addEventListener("click", (e) => {
+    e.stopPropagation();
+    openCloseModal();
+})
+
+
+
+class Card {
+    constructor(name, phone, email, address, token) {
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+        this.address = address;
+        this.token = token;
     }
+}
 
-    openModalButton.addEventListener("click", (e) => {
-        e.stopPropagation();
-        openCloseModal();
-    })
-    modalBox.addEventListener("click", (e) => {
-        e.stopPropagation();
-    })
-    closeModalButton.addEventListener("click", (e) => {
-        e.stopPropagation();
-        openCloseModal();
-    })
-    addContactModal.addEventListener("click", (e) => {
-        e.stopPropagation();
-        openCloseModal();
-    })
+function addContactInfo(name, phone, email, address, token) {
+    let newCard = new Card(name, phone, email, address, token);
+    allCards.push(newCard);
+}
 
-    let allCards = [];
+function createCards(data) {
 
-    class Card {
-        constructor(name, phone, email, address) {
-            this.name = name;
-            this.phone = phone;
-            this.email = email;
-            this.address = address;
-        }
-    }
+    contactsContainer.textContent = '';
 
-    function addContactInfo (name, phone, email, address) {
-        let newCard = new Card(name, phone, email, address);
-        allCards.push(newCard);
-    }
-
-    function createCards(data) {
-
+    for(let prop of data) {
+        
         const card = contactCardTemplate.content.cloneNode(true).children[0];
 
         const nameContactCard = card.querySelector("[data-name]");
@@ -64,68 +70,42 @@
         const emailContactCard = card.querySelector("[data-email]");
         const addressContactCard = card.querySelector("[data-address]");
 
-        nameContactCard.textContent = data[data.length -1].name;
-        phoneContactCard.textContent = data[data.length -1].phone;
-        emailContactCard.textContent = data[data.length -1].email;
-        addressContactCard.textContent = data[data.length -1].address;
+        nameContactCard.textContent = prop.name;
+        phoneContactCard.textContent = prop.phone;
+        emailContactCard.textContent = prop.email;
+        addressContactCard.textContent = prop.address;
+        card.dataset.idCard = prop.token;
 
         contactsContainer.appendChild(card);
-
     }
 
-    formNewContact.addEventListener("submit", (e) => {
-        e.stopPropagation();
-        e.preventDefault();
+}
 
-        let formInputs = formNewContact.querySelectorAll("input");
+formNewContact.addEventListener("submit", (e) => {
+    e.stopPropagation();
+    e.preventDefault();
 
-        if (!formNewContact.checkValidity()) {
+    let formInputs = formNewContact.querySelectorAll("input");
 
-            formNewContact.classList.add('was-validated');
+    if (!formNewContact.checkValidity()) {
+        formNewContact.classList.add('was-validated');
+    } else {
 
-        } else {
-            let nameValue = newName.value;
-            let phoneValue = newPhone.value;
-            let emailValue = newEmail.value;
-            let addressValue = newAddress.value;
-            addContactInfo(nameValue, phoneValue, emailValue, addressValue);
-            formInputs.forEach(input => input.value = '');
-            formNewContact.classList.remove('was-validated');
-            openCloseModal();
-            createCards(allCards);
-            console.log(allCards);
-            console.log(allCards[0]);
-            console.log(allCards[0].name);
-        }
-        
-    })
+        let nameValue = newName.value;
+        let phoneValue = newPhone.value;
+        let emailValue = newEmail.value;
+        let addressValue = newAddress.value;
+        let tokenValue = tokenId();
 
-    // deleteContactBtn.addEventListener("click", (e) => {
-    //     e.stopPropagation();
-    //     console.log("");
-    // })
+        addContactInfo(nameValue, phoneValue, emailValue, addressValue, tokenValue);
+        createCards(allCards);
+        formInputs.forEach(input => input.value = '');
+        formNewContact.classList.remove('was-validated');
+        openCloseModal();
+    }
 
+})
 
-    
-
-})()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+function tokenId() {
+    return (Math.floor(Math.random() * (9999999999999 + 1)) + Date.now());
+}
